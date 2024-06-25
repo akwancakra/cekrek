2e9e66f9959fb7cc60793828b985a93d85b98df5
akwancakra <akwancakra20@gmail.com>
2024-06-25 12:27:50 +0700

    chore: Update next.config.mjs and page.tsx

--- src/app/t/students/[id]/assessment/page.tsx
+++ src/app/t/students/[id]/assessment/page.tsx
@@ -1,5 +1,6 @@
 import AssessmentChoice from "@/components/elements/assessments/AssessmentChoice";
+import AssessmentGetRecomendations from "@/components/elements/assessments/AssessmentGetRecomendations";
 
 export default function Assessment() {
-    return <AssessmentChoice />;
+    return <AssessmentGetRecomendations />;
 }
